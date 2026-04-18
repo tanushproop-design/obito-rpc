aff96ce0f7d1d91ce7f20784e1e778a644d34f4e
tanushproop-design <tanushproop@gmail.com>
2026-04-18 20:18:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,120 @@
+// ==========================================================
+// YAHAN SAB KUCH CHANGE KARO (CONFIG FILE)
+// MULTIPLE PROFILES SUPPORTED - Har id ke liye alag detail
+// ==========================================================
+
+module.exports = [
+    // --------------------------------------------------------
+    // PROFILE 1 (Pehli id / token ke liye)
+    // --------------------------------------------------------
+    {
+        activityType: "PLAYING",
+       name: "𝙏𝙖𝙣𝙪𝙨𝙝 𝙭 𝙀𝙨𝙥𝙖𝙙𝙖",
+        details: "1% 1%",
+        state: "ᴊᴏɪɴ ᴇꜱᴘᴀᴅᴀ",
+        timeHours: 500000,
+        
+        // IMPORTANT: Use Imgur/Tenor link here
+        largeImage: "https://cdn.discordapp.com/attachments/1477725989718065233/1493028442873135204/From_KlickPin_CF_Pin_em_photo_ideas_-_Pin-53761789299357777_1.gif?ex=69dd7a97&is=69dc2917&hm=07c526949f9da9e9f1fcef06909428c568461a5ed5d5657f85e9f4a6cc70803f&",
+        largeImageText: "𝙏𝙖𝙣𝙪𝙨𝙝 𝙭 𝙀𝙨𝙥𝙖𝙙𝙖",
+        
+        smallImage: "https://cdn.discordapp.com/attachments/1423419016256688233/1486813756355903538/From_Main_Klickpin_CF-_tenorcom_-_GccXZqrWe.gif?ex=69db4df7&is=69d9fc77&hm=54821980fd87cf0fb9b9b805148853871cf2194f0a2c7bbb0bfb9e1f8de04092&",
+        smallImageText: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        
+        button1Name: "𝙀𝙨𝙥𝙖𝙙𝙖",
+        button1URL: "https://espada.work.gd/",
+        button2Name: "𝙖𝙗𝙤𝙪𝙩 𝙢𝙚",
+        button2URL: "https://guns.lol/fn.exe",
+    },
+
+    // --------------------------------------------------------
+    // PROFILE 2 (Doosri id / token ke liye)
+    // Agar teesri id hui aur yahan sirf 2 profile hain, toh teesri ID AUTOMATICALLY Profile 1 le legi!
+    // --------------------------------------------------------
+    {
+        activityType: "WATCHING",
+        name: "𝙣𝙚𝙭𝙖𝙛𝙮𝙧𝙚𝙯",
+        details: "exe.js",
+        state: "lol",
+        timeHours: 500000,
+        
+        // Alag Image yahan daalein Imgur se
+        largeImage: "https://cdn.discordapp.com/attachments/1477725989718065233/1492939589726245115/From_KlickPin_CF_Hey_Video_in_2026___Inspo_Hey_-_Pin-54535845482023158.gif?ex=69dd27d7&is=69dbd657&hm=bffc419d197dcb1ea882510c9c5a70d59dfd96a4afb77db30b844972fd436ff9&",
+        largeImageText: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        
+        smallImage: "https://cdn.discordapp.com/attachments/1423419016256688233/1486813756355903538/From_Main_Klickpin_CF-_tenorcom_-_GccXZqrWe.gif?ex=69db4df7&is=69d9fc77&hm=54821980fd87cf0fb9b9b805148853871cf2194f0a2c7bbb0bfb9e1f8de04092&",
+        smallImageText: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        
+        button1Name: "nexafyrez web",
+        button1URL: "https://nexafyrez.vercel.app/",
+        button2Name: "",
+        button2URL: "",
+    },
+
+    // --------------------------------------------------------
+    // PROFILE 3 (Teesri id / token ke liye)
+    // --------------------------------------------------------
+    {
+        activityType: "LISTENING",
+        name: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        details: "𝙑𝙞𝙗𝙞𝙣𝙜 𝙩𝙤 𝙢𝙪𝙨𝙞𝙘",
+        state: "Spotify",
+        timeHours: 500000,
+        
+        // Image URL yahan daalein
+        largeImage: "https://cdn.discordapp.com/attachments/1477725989718065233/1492941834325069925/From_KlickPin_CF_savefollow_ig___quyethgg_trong_2026.gif?ex=69dd29ee&is=69dbd86e&hm=49f87a84e85d3952ed8cf304d6ec2dd6a577c5c6b982800fe58b7f6000749e3f&",
+        largeImageText: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        
+        smallImage: "https://cdn.discordapp.com/attachments/1423419016256688233/1486813756355903538/From_Main_Klickpin_CF-_tenorcom_-_GccXZqrWe.gif?ex=69db4df7&is=69d9fc77&hm=54821980fd87cf0fb9b9b805148853871cf2194f0a2c7bbb0bfb9e1f8de04092&",
+        smallImageText: "𝙁𝙪𝙘𝙠 𝙮𝙤𝙪",
+        
+        button1Name: "Listen Along",
+        button1URL: "https://open.spotify.com/user/31qsgovvwp4jjhysh6t7gnf2yxme",
+        button2Name: "",
+        button2URL: "",
+    },
+    // --------------------------------------------------------
+    // PROFILE 4 (Chauthi id / token ke liye)
+    // --------------------------------------------------------
+    {
+        activityType: "COMPETING",
+        name: "Profile 4",
+        details: "Details 4",
+        state: "State 4",
+        timeHours: 500000,
+        
+        largeImage: "https://cdn.discordapp.com/attachments/1477725989718065233/1493027908598628513/From_KlickPin_CF_Pin_on_Looks_1.gif?ex=69dd7a18&is=69dc2898&hm=ee5652aabd19ee153e21cf363280dd09a4aa5cb85b96f55ca52bcae2a121264b&",
+        largeImageText: "Profile 4",
+        
+        smallImage: "",
+        smallImageText: "",
+        
+        button1Name: "Link 1",
+        button1URL: "https://discord.com",
+        button2Name: "",
+        button2URL: "",
+    },
+
+    // --------------------------------------------------------
+    // PROFILE 5 (Paanchvi id / token ke liye)
+    // --------------------------------------------------------
+    {
+        activityType: "STREAMING",
+        name: "Profile 5",
+        details: "Details 5",
+        state: "State 5",
+        timeHours: 500000,
+        
+        largeImage: "https://cdn.discordapp.com/attachments/1477725989718065233/1493028442873135204/From_KlickPin_CF_Pin_em_photo_ideas_-_Pin-53761789299357777_1.gif?ex=69dd7a97&is=69dc2917&hm=07c526949f9da9e9f1fcef06909428c568461a5ed5d5657f85e9f4a6cc70803f&",
+        largeImageText: "Profile 5",
+        
+        smallImage: "",
+        smallImageText: "",
+        
+        button1Name: "Link 1",
+        button1URL: "https://discord.com",
+        button2Name: "",
+        button2URL: "",
+        url: "https://twitch.tv/discord" // Streaming URL yahan daalein
+    }
+];
